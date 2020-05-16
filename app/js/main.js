1c9260f04aa954a2a3843f827c0b2df6c0b4daef
Fedichkin Denis <fedichkin.dy@gmail.com>
2020-05-16 22:05:56 +0300

    add section products

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(".products__inner-item").slick({
+        arrows: true,
+        dots: false,
+        prevArrow: '<button class="slick-arrow slick-prev"><i class="lnr lnr-chevron-left"></i></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><i class="lnr lnr-chevron-right"></i></button>'
+    });
+});
